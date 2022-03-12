9455a871bbf4114ca03c813f92392692eb6b3608
Tanvir <tanvirahmed1418@gmail.com>
2022-03-12 19:34:17 +0600

    Filter Component Finished

--- src/Components/Delivery/index.js
+++ src/Components/Delivery/index.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import Filters from '../Common/Filters';
 import './delivery.css';
 
+const deliveryFilters = [
+  {
+    id: 1,
+    icon: <i className="fi fi-rs-settings-sliders absolute-center"></i>,
+    title: 'Filters',
+  },
+  {
+    id: 2,
+    title: 'Rating: 4.0+',
+  },
+  {
+    id: 3,
+    title: 'Safe and Hygienic',
+  },
+  {
+    id: 4,
+    title: 'Pure Veg',
+  },
+  {
+    id: 5,
+    icon: <i className="fi fi-rs-apps-sort absolute-center"></i>,
+    title: 'Delivery Time',
+  },
+  {
+    id: 6,
+    title: 'Great Offers',
+  },
+];
+
 const Delivery = () => {
-  return <div>This is Delivery</div>;
+  return (
+    <div>
+      <div className="max-width">
+        <Filters filterList={deliveryFilters} />
+      </div>
+    </div>
+  );
 };
 
 export default Delivery;
